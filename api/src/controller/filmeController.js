dc26c84791e937f65a99a048b6303610005f1c72
kauanny silva <kauannysilvaluz.kl@gmail.com>
2022-06-01 14:21:08 -0300

    subindoooooo

---
+++ api/src/controller/filmeController.js
@@ -0,0 +1,25 @@
+
+import { inserirFilme } from '../repository/filmeRepository.js'
+
+import { Router } from 'express'
+const server = Router();
+
+
+server.post('/filme', async (req,resp) => {
+    try{
+        const novoFilme = req.body;
+
+        const filmeInserido = await inserirFilme(novoFilme);
+
+        resp.send(filmeInserido);
+    }catch (err) {
+        resp.send(400).send({
+            erro: err.message
+        })
+    }
+})
+
+
+
+
+export default server;
